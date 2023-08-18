de6506a2546a818fdc0cb63943c1bf013fc538e1
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-08-18 15:43:53 +0500

    Initial Commit

---
+++ Subs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Subs
+//
+//  Created by Anton Vikhlyaev on 18.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
