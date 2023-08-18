72d4b664eb25cd3951e3a00e20b0b9db6cf4d963
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-08-18 15:53:30 +0500

    Changes folder structure

---
+++ Subs/App/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Subs
+//
+//  Created by Anton Vikhlyaev on 18.08.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
