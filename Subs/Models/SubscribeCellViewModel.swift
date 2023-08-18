095c657ca396f7ffa3e4cbe6d270cd4008ab0909
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-08-18 22:42:39 +0500

    Adds SubscribeCell view model

---
+++ Subs/Models/SubscribeCellViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  SubscribeCellViewModel.swift
+//  Subs
+//
+//  Created by Anton Vikhlyaev on 18.08.2023.
+//
+
+import Foundation
+
+struct SubscribeCellViewModel {
+    let title: String
+    let cost: String
+    let currency: String
+    let nextPayment: String
+    let reminder: String
+}
