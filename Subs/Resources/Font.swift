d5df5f973ada4b325b927e85728c9eea574d7660
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-08-18 22:37:01 +0500

    Adds font constants

---
+++ Subs/Resources/Font.swift
@@ -0,0 +1,14 @@
+//
+//  Font.swift
+//  Subs
+//
+//  Created by Anton Vikhlyaev on 18.08.2023.
+//
+
+import UIKit
+
+extension UIFont {
+    static let regular17 = UIFont.systemFont(ofSize: 17)
+    static let regular15 = UIFont.systemFont(ofSize: 15)
+    static let medium17 = UIFont.systemFont(ofSize: 17, weight: .medium)
+}
