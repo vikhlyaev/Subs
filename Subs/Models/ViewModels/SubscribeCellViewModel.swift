3e70da8ec39eced930dba1ff3a2a8c001c37a235
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-08-19 13:51:01 +0500

    Adds formatters and custom init

--- Subs/Models/ViewModels/SubscribeCellViewModel.swift
+++ Subs/Models/ViewModels/SubscribeCellViewModel.swift
@@ -9,24 +9,42 @@ import Foundation
 
 struct SubscribeCellViewModel {
     
-    static private let formatter: NumberFormatter = {
+    // MARK: - Formatters
+    
+    static private let currencyFormatter: NumberFormatter = {
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
         formatter.locale = Locale.current
         return formatter
     }()
     
-    static func convert(subscribe: Subscribe) -> SubscribeCellViewModel {
-        SubscribeCellViewModel(
-            title: subscribe.title,
-            cost: formatter.string(from: subscribe.cost as NSNumber) ?? "",
-            nextPayment: "завтра",
-            reminder: "за 3 дня"
-        )
-    }
+    static private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale.current
+        formatter.dateFormat = "d MMMM yyyy"
+        return formatter
+    }()
+    
+    // MARK: - Properties
     
     let title: String
     let cost: String
     let nextPayment: String
     let reminder: String
+    
+    // MARK: - Life Cycle
+    
+    init(title: String, cost: String, nextPayment: String, reminder: String) {
+        self.title = title
+        self.cost = cost
+        self.nextPayment = nextPayment
+        self.reminder = reminder
+    }
+    
+    init(subscribe: Subscribe) {
+        self.title = subscribe.title
+        self.cost = SubscribeCellViewModel.currencyFormatter.string(from: subscribe.cost as NSNumber) ?? ""
+        self.nextPayment = SubscribeCellViewModel.dateFormatter.string(from: subscribe.nextPayment)
+        self.reminder = subscribe.reminder.title
+    }
 }
